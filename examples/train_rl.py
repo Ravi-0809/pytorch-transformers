2c8cf66a98df533f7934d8f7c9a9c4fca5c275f8
Ravi-0809 <crbharadwaj@yahoo.com>
2019-08-21 16:51:42 +0530

    minor fix 3.0

--- examples/train_rl.py
+++ examples/train_rl.py
@@ -628,10 +628,11 @@ def main():
         args.n_gpu = torch.cuda.device_count()
         torch.distributed.init_process_group(backend='nccl')
     else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+        logger.info('------------------------------------NOT SUPPOSED TO BE HERE------------------------------------')
         torch.cuda.set_device(args.local_rank)
         device = torch.device("cuda", args.local_rank)
         torch.distributed.init_process_group(backend='nccl')
-        args.n_gpu = 1
+        args.n_gpu = 2
     args.device = device
 
     # Setup logging
